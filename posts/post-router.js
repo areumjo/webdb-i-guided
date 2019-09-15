5fcfd04669e0f52fb6196bfcd6083267a9d5202d
Areum Jo <areumjo1@gmail.com>
2019-09-15 14:57:39 -0700

    Added knex, sqlite3 and created get endpoints

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,11 +6,34 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    // SELECT * FROM Post (SQL)
+    // db.select('*').from('posts');
+    db.select('posts')
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get post' })
+    })
 });
 
 router.get('/:id', (req, res) => {
-
+    // SELECT * FROM posts WHERE id = param.id (SQL)
+    const { id } = req.params;
+    db.select('post').where({ id })
+    .then(posts => {
+        console.log('post: ', post); // multiple objects
+        const post = posts[0];
+        if (post) {
+            res.json(posts);
+        } else {
+            res.status(404).json({ message: 'invalid post id' })
+        }
+        
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get post by id'})
+    })
 });
 
 router.post('/', (req, res) => {
